72d6f64dd6e1f4b073e8c8bcd7bc5b5af9ad2241
Pavan-9991 <156654179+Pavan-9991@users.noreply.github.com>
2024-05-17 11:00:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+	var FirstName = document.elementbyId(fname);
+	var LastName = document.elementbyId(lname);
+
+	alert(FirstName + LastName)
 
 }
